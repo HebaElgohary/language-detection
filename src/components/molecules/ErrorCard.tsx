196566adddd993aba5abbeb7d3fa84f72545443b
HebaElgohary <hebasamir535@gmail.com>
2026-05-01 15:38:15 +0300

    fix

--- src/components/molecules/ErrorCard.tsx
+++ src/components/molecules/ErrorCard.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { useTranslations } from 'next-intl'
 export default function ErrorCard({error}:any) {
   const t=useTranslations('errorCard')
-  console.log(error)
+  console.log(' in error card',error)
   return (
     <div className='w-full !my-14 md:w-150 !mx-auto text-sm md:text-lg h-25 flex items-center border-red-800 border-1 rounded-2xl  ds-bg-secondary-200 ds-text-secondary !p-5'>
-  <span className='w-5 h-5 !p-5 text-lg md:text-xl  !mx-2 flex justify-center items-center  rounded-full border border-red-800 '>!</span>{error.message || t('p')}
+  <span className='w-5 h-5 !p-5 text-lg md:text-xl  !mx-2 flex justify-center items-center  rounded-full border border-red-800 '>!</span>{error && t('p')}
     </div>
   )
 }
