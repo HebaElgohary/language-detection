237787fd7508805c091d53a2b1461ba1e47563d2
HebaElgohary <hebasamir535@gmail.com>
2026-05-01 14:58:02 +0300

    feaat: add translation to langCard & errorCard

--- src/components/molecules/ErrorCard.tsx
+++ src/components/molecules/ErrorCard.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import { useTranslations } from 'next-intl'
 export default function ErrorCard() {
+  const t=useTranslations('errorCard')
   return (
     <div className='w-full !my-14 md:w-150 !mx-auto text-sm md:text-lg h-25 flex items-center border-red-800 border-1 rounded-2xl  ds-bg-secondary-200 ds-text-secondary !p-5'>
-  <span className='w-5 h-5 !p-5 text-lg md:text-xl  !mx-2 flex justify-center items-center  rounded-full border border-red-800 '>!</span>Unable to detect language
+  <span className='w-5 h-5 !p-5 text-lg md:text-xl  !mx-2 flex justify-center items-center  rounded-full border border-red-800 '>!</span>{t('p')}
     </div>
   )
 }
--- src/components/molecules/FlagCard.tsx
+++ src/components/molecules/FlagCard.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import Images from '../atoms/Images'
+import { useTranslations } from 'next-intl'
 
 export default function FlagCard({lang}:{lang:string}) {
+      const t=useTranslations('langCard')
+    
   return (
     <div className='flex flex-col md:flex-row border gap-3 border-dotted rounded-xl w-full md:w-150 !mx-auto !p-3 justify-start !my-11'>
         <div className='w-1/3'>
@@ -9,8 +12,8 @@ export default function FlagCard({lang}:{lang:string}) {
 
         </div>
     <div className='flex flex-col   '>
-        <span className='text-lg font-semibold '>Detected Language</span>
-        <span>{lang}</span>
+        <span className='text-lg font-semibold '>{t('title')}</span>
+        <span>{lang && t('lang')}</span>
 
     </div>
 
